cb95b2cc861c3c897de71efa9d07014529aa46e0
Dellshams <ayodelesaba@gmail.com>
2020-05-06 06:47:35 +0100

    update auth.js with login

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
 
-const { signUp } = require("../controllers/userAuth");
+const { signUp, logIn } = require("../controllers/userAuth");
 
 
 router.post('/signup', signUp);
+router.post("/login", logIn);
 
 module.exports = router;
