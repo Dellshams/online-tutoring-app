9473265d4bea3e9d6e4d457fc776ce2704ffd6c6
Dellshams <ayodelesaba@gmail.com>
2020-05-07 03:54:42 +0100

    create genAuth

--- controllers/genAuth.js
+++ controllers/genAuth.js
@@ -1,1 +1,61 @@
-const User = require("../models/userModel");
+const User = require("../models/user");
+const Subject = require("../models/subjectModel");
+const Category= require("../models/categoryModel");
+
+exports.getSubjectInCategoryById = (req, res, next) =>{
+    const subjectId = req.body;
+    const categoryName = req.body;
+
+    Category.findOne({ name: categoryName })
+    .populate({ path: "subjects", match: { _id: subjectId }})
+    .then( subject => {
+        if(!subject){
+            return res.status(404)
+            .send({ status: false, message: "Subject not found" })
+        }
+        res.status(200).send({ message:"subject found", subject: subjects })
+    })
+    .catch( err => console.log (err))
+}
+
+exports.getAllSubjectsInACategory = (req, res, next) => {
+    const categoryName = req.body;
+
+    Category.findOne({ name: categoryName })
+    .populate("subjects")
+    .then( subjects => {
+        res.status(200)
+        .send({ message: subjects})
+    })
+    .catch( err => console.log (err))
+}
+
+exports.getAllCategories = (req, res, next) => {
+
+    Category.find({})
+    .then( categories => {
+        res.status(200)
+        .send({ status: true, message: categories })
+    })
+    .catch( err => console.log (err))
+}
+
+  exports.searchForSubjects = (req,res, next) => {
+
+    Subject.find().sort({ name : 1 })
+    .then( found =>{
+        res.status(200)
+        .send({ status: true, message: found })
+    })
+    .catch( err => console.log (err))
+}
+
+// exports.searchForTutors = (req,res, next) => {
+
+//     ------.find().sort({ firstName : 1 })
+//     .then( found =>{
+//         res.status(200)
+//         .send({ status: true, message: found })
+//     })
+//     .catch( err => console.log (err))
+// }
