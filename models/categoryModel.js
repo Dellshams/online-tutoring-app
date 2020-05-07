4ea8945578a887b246683ce06bd90be2f3ea4b44
Dellshams <ayodelesaba@gmail.com>
2020-05-07 01:39:34 +0100

    restructure models

--- models/categoryModel.js
+++ models/categoryModel.js
@@ -1,16 +1,3 @@
-<<<<<<< Updated upstream
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const categorySchema = new Schema({
-
-    name: { type: String, required: true, enum: ['primary','jss','sss'] },
-    subjects: [ { type: mongoose.Schema.Types.ObjectId, ref: 'Subject' } ]
-
-},  {timestamps: true});
-
-module.exports = mongoose.model('Category', categorySchema);
-=======
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
@@ -21,4 +8,3 @@ const categorySchema = new Schema({
 },  {timestamps: true});
 
 module.exports = mongoose.model("Category", categorySchema);
->>>>>>> Stashed changes
--- models/subjectModel.js
+++ models/subjectModel.js
@@ -2,16 +2,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const subjectSchema = new Schema({
-<<<<<<< Updated upstream
-
-    name: { type: String, required: true },
-    tutors: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category' },
-=======
     name: { type: String, required: true },
     tutors: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
     category: { type: String, required: true }
->>>>>>> Stashed changes
 
 },  {timestamps: true});
 
