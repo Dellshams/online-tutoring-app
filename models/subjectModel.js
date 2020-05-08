5e21c09c814576a92bbd20e3dad770fd913df928
Dellshams <ayodelesaba@gmail.com>
2020-05-08 02:59:38 +0100

    update

--- models/subjectModel.js
+++ models/subjectModel.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const subjectSchema = new Schema({
-    name: { type: String, required: true },
+    subjectName: { type: String, required: true },
     tutors: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-    category: { type: String, required: true }
+    category: { type: String }
 
 },  {timestamps: true});
 
