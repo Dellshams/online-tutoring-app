3badaccc9935b1f1e3618948f3b0104982172f92
Dellshams <ayodelesaba@gmail.com>
2020-05-08 22:46:27 +0100

    update

--- routes/tutorRoute.js
+++ routes/tutorRoute.js
@@ -1,1 +1,12 @@
-const router = require("express").Router();
+//for tutor access only
+const router = require("express").Router()
+const tutorAuth = require("../controllers/tutorAuth")
+const userAuth = require("../controllers/userAuth")
+
+
+
+
+
+
+
+module.exports = router;
