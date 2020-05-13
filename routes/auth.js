bf804031a0e4725af89cfcefb7a8e3bb034707fe
Dellshams <ayodelesaba@gmail.com>
2020-05-13 20:10:13 +0100

    update

--- routes/auth.js
+++ routes/auth.js
@@ -6,4 +6,4 @@ const { signUp, logIn } = require("../controllers/userAuth");
 router.post("/api/v1/signup", signUp);
 router.post("/api/v1/login", logIn);
 
-module.exports = router;
+module.exports = router
