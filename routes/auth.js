d700ef9e84a58f096d5f3f6e4082f19783feab0f
Dellshams <ayodelesaba@gmail.com>
2020-05-13 20:20:39 +0100

    auth

---
+++ routes/auth.js
@@ -0,0 +1,9 @@
+const router = require("express").Router();
+
+const { signUp, logIn } = require("../controllers/userAuth");
+
+
+router.post("/api/v1/signup", signUp);
+router.post("/api/v1/login", logIn);
+
+module.exports = router;
