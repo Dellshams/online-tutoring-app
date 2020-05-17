155159f8466bac4aefb5dc3e06c5e80b570cb94a
Dellshams <ayodelesaba@gmail.com>
2020-05-17 12:34:24 +0100

    update

--- models/subjectModel.js
+++ models/subjectModel.js
@@ -2,17 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const subjectSchema = new Schema({
-<<<<<<< Updated upstream
-    subjectName: { type: String, required: true },
-    tutors: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-    category: { type: String }
-=======
 
     subjectName: { type: String, required: true },
     categoryName: { type: String, required: true },
     tutors: [{ type: Schema.Types.ObjectId, ref: "User" }]
-    
->>>>>>> Stashed changes
 
 },  {timestamps: true});
 
